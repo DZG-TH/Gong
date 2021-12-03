9c72602a2e26d8efd269e4f3a981f0aee5f35f82
hannses <hannsesh.sharemail@gmail.com>
2021-12-03 21:01:18 +0100

    Added a UI can communicate with a Server

--- gui/scripts/WeekTemplate.gd
+++ gui/scripts/WeekTemplate.gd
@@ -1,9 +1,11 @@
 extends OptionButton
 
-
 func _ready():
 	var templates = ServerCommunicator.get_week_templates()
-	var item_selected = ServerCommunicator.get_template_week(get_parent().get_parent().KW)
+	var parent_parent = get_parent().get_parent()
+	if !parent_parent.name.is_valid_integer():
+		parent_parent = parent_parent.get_parent()
+	var item_selected = ServerCommunicator.get_template_week(int(parent_parent.KW))
 	var found = false
 	var counter = 0
 	for template in templates:
@@ -17,3 +19,6 @@ func _ready():
 		add_item("Custom", counter + 2)
 		select(get_item_index(counter + 2))
 	
+
+func _item_selected(index: int):
+	ServerCommunicator.set_template_week(get_parent().get_parent().KW, get_item_text(index))
