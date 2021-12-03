5c32aafc00d9c6132cd156767da05b9d6aebc4e7
hannses <hannsesh.sharemail@gmail.com>
2021-12-03 21:04:10 +0100

    made a script to initialize the filesystem

---
+++ init.py
@@ -0,0 +1,12 @@
+import os
+
+def init_current_config():
+    for week in range(53):
+        if not os.path.isdir("./current_config/"+str(week+1)):
+            print("./current_config/"+str(week+1))
+            os.mkdir("./current_config/"+str(week+1))
+        for day in range(5):
+            print("./current_config/" + str(week+1)+ "/"+ str(day+1)+".day")
+            open("./current_config/" + str(week+1)+ "/"+ str(day+1)+".day","a").close()
+
+init_current_config()
